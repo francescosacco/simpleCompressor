54b8bcc7bf6f09528658e49f5aaaa396df7ab2b8
Francesco Sacco <francesco_sacco@hotmail.com>
2021-11-16 22:30:10 -0300

    Add first source files.

---
+++ inc/histogram.h
@@ -0,0 +1,17 @@
+#ifndef _HISTOGRAM_H_
+#define _HISTOGRAM_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+    }
+#endif
+
+#endif // _HISTOGRAM_H_
---
+++ src/histogram.c
@@ -0,0 +1,1 @@
+#include "hostogram.h"
