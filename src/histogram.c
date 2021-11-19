56fcb9d5158a9a1bae9e712c48f66f1d1d5bd82b
Francesco Sacco <francesco_sacco@hotmail.com>
2021-11-18 21:38:14 -0300

    Add histogram.

--- src/histogram.c
+++ src/histogram.c
@@ -1,11 +1,5 @@
 #include "histogram.h"
 
-typedef struct _HISTOGRAM_DATACALC_T_
-{
-    uint8_t  data ;
-    uint32_t frequency ;
-} histogram_dataCalc_t ;
-
 void histogram_calculation( uint8_t * bufferIn , size_t bufferSize , histogram_dataCalc_t * histogram_dataCalc )
 {
     if( bufferIn == ( uint8_t * ) NULL )
@@ -18,6 +12,11 @@ void histogram_calculation( uint8_t * bufferIn , size_t bufferSize , histogram_d
         return ;
     }
     
+    if( bufferSize == 0 )
+    {
+        return ;
+    }
+
     for( uint16_t i = 0 ; i < 256 ; i++ )
     {
         histogram_dataCalc[ i ].data = ( uint8_t ) i ;
